acf58eb18cdf2dfb9089380210d6d3e1df97797c
Raghad Hanon <raghadhanoon2015@gmail.com>
2024-10-30 15:03:48 +0200

    Add Project

---
+++ Inventory_Management_System/Configuration/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Inventory_Management_System.Configurations;
+public class Configuration : IConfiguration
+{
+    public IConfigurationRoot Configurations { get; }
+    public Configuration()
+    {
+        Configurations = SetConfiguration();
+    }
+
+    public IConfigurationRoot SetConfiguration()
+    {
+        return new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("AppSetting.json")
+                    .Build();
+    }
+}
